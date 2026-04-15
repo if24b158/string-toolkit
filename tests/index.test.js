947d8517ea7d89994b8d2fc7204eb013e8fa3224
Kyrillos Abdalla <if24b158@technikum-wien.at>
2026-04-15 03:10:21 +0200

    Add toKebabCase tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,9 @@
-const { toSnakeCase } = require("../src/index");
+const { toSnakeCase, toKebabCase } = require("../src/index");
 
 test("converts a string to snake_case", () => {
     expect(toSnakeCase("Hello World")).toBe("hello_world");
+});
+
+test("converts a string to kebab-case", () => {
+    expect(toKebabCase("Hello World")).toBe("hello-world");
 });
